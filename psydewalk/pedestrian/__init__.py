000675347acd5223cffc92634533abcc0cff0e29
Tobias Schramm <tobleminer@gmail.com>
2016-07-31 19:57:34 +0200

    Waypointz!

--- psydewalk/pedestrian/__init__.py
+++ psydewalk/pedestrian/__init__.py
@@ -2,25 +2,18 @@ from ..entity import SmoothMovingEntity
 from ..jitter import Jitter
 from ..data import DataProvider
 
+from asyncio import Queue
+from threading import Thread
+
 class Pedestrian(SmoothMovingEntity, DataProvider, Jitter):
 	def __init__(self):
 		DataProvider.__init__(self)
 		Jitter.__init__(self)
 		SmoothMovingEntity.__init__(self)
+		self.waypoints = Queue()
 
-	def move(self, interval=100):
-		while self.loc.dist(self.dest) > self.DEST_MAX_DIST:
-			if(self.speeddelta > 0):
-				self.speed += (1 - 1 / abs(self.speeddelta)) ** self.speedaproxcnt
-			else:
-				self.speed -= (1 - 1 / abs(self.speeddelta)) ** self.speedaproxcnt
-			self.speedaproxcnt += 1
-			dist = self.loc.dist(self.dest)
-			delta = self.dest - self.loc
-			vect = delta / dist
-			if dist > self.speed * interval / 1000:
-				vect = vect * (self.speed * interval / 1000);
-			else:
-				vect = vect * self.DEST_MAX_DIST
-			self.loc = self.loc + vect
-			sleep(interval / 1000)
+	def run(self, interval=100):
+		while True:
+			waypoint = self.waypoints.get()
+			self.moveTo(waypoint, interval)
+			
